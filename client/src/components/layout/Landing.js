c49ba6679a071010b65f526e20fa747c1da216df
sondaraksa <son_daraksa@hotmail.com>
2019-02-05 15:20:39 +0700

    create client init version 1

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <div className="landing">
+          <div className="dark-overlay landing-inner text-light">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 text-center">
+                  <h1 className="display-3 mb-4">Welcome Mr Son Daraksa</h1>
+                  <p className="lead">
+                    {" "}
+                    Please have a good time creating new project. You do not
+                    have much time. Do it now or you will regret when it is too
+                    late.
+                  </p>
+                  <hr />
+                  <Link to="/register" className="btn btn-lg btn-info mr-2">
+                    Sign Up
+                  </Link>
+                  <Link to="/login" className="btn btn-lg btn-light">
+                    Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
